b36b4bcd8a1daf5012511866c4763e866820a7a4
Rounakazadhere <rounakazad09@gmail.com>
2026-01-02 13:28:15 +0530

    Add vercel config

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.2pkraqj3apo"
+    "revision": "0.2d67d2io3g4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
